7d35a0bf7b41096b00ca8e2ccb013eb9c93a1768
ayyappa <ayyappaswamy50@gmail.com>
2026-06-06 23:16:13 +0530

    Fix Vercel build and GitHub Pages routing issues

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect, useCallback } from "react";
 import { AddItem } from "./components/AddItem";
 import ProductList from "./components/ProductList";
